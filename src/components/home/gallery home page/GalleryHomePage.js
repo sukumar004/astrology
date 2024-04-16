02c3472f630d0ea700fc206dcede1870d519e14d
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-04-16 22:43:03 +0530

    Update GalleryHomePage.js and galleryHomePage.css

--- src/components/home/gallery home page/GalleryHomePage.js
+++ src/components/home/gallery home page/GalleryHomePage.js
@@ -3,14 +3,13 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./galleryHomePage.css";
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function GalleryHomePage() {
-
-  useEffect(()=>{
-    Aos.init()
-},[])
+  useEffect(() => {
+    Aos.init();
+  }, []);
 
   const imgPath = [
     "/images/img-1.jpg",
@@ -26,21 +25,27 @@ function GalleryHomePage() {
     "/images/img-2.jpg",
     "/images/img-3.jpg",
   ];
-
+  var isMobile =
+    Math.min(window.screen.width, window.screen.height) < 768 ||
+    navigator.userAgent.indexOf("Mobi") > -1;
   var settings = {
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: isMobile ? 1 : 3,
     slidesToScroll: 1,
-    dots:true
+    dots: true,
   };
-
 
   // console.log("screenWidth : ",screenWidth)
 
   const imgSliderShow = imgPath.map((img, index) => {
     return (
-      <div className="galleryHomePage-imgage-slider" key={index} data-aos='fade-up' data-aos-duration='1500'>
+      <div
+        className="galleryHomePage-imgage-slider"
+        key={index}
+        data-aos="fade-up"
+        data-aos-duration="1500"
+      >
         <img src={img} alt="image" />
       </div>
     );
@@ -48,12 +53,12 @@ function GalleryHomePage() {
 
   return (
     <div className="galleryHomePage-parent">
-
       <div className="gallerrHomePage-title">
-      <h1 data-aos="zoom-up" data-aos-duration='1500'>Gallery</h1>
+        <h1 data-aos="zoom-up" data-aos-duration="1500">
+          Gallery
+        </h1>
         <img src="/svg/divider1.svg" alt="divider" id="divider-gallery-id" />
       </div>
-
 
       <Slider {...settings} className="Slider">
         {imgSliderShow}
